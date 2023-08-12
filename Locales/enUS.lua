959922f273aebdf5d215e698d4223ea9046adea2
Slothpala <slothpala@gmail.com>
2023-08-12 03:07:49 +0200

    locales update

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,5 +1,42 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("HealthBarColor", "enUS", true)
 
---Core--
+--Modules
+--Health Bars
 L["Health Bars"] = true
-	
+--Fonts
+L["Fonts"] = true
+L["Font Settings"] = true
+L["Outline"] = true
+L["outline_option_1"] = "OUTLINE"
+L["outline_option_2"] = "THICKOUTLINE"
+L["outline_option_3"] = "MONOCHROME"
+L["outline_option_4"] = "NONE"
+--Units
+L["Player"] = true
+L["Target"] = true
+L["Target of Target"] = true
+L["Focus"] = true
+L["Target of Focus"] = true
+L["Pet"] = true
+L["Boss"] = true
+--Color options
+L["color"] = true
+L["NPC color"] = true
+L["NPC color by reaction"] = true
+L["color_options"] = "color by.."
+L["targets class color"] = true
+L["tot class color"] = true
+L["focus class color"] = true
+L["tof class color"] = true
+L["color_option_2"] = "my class color"
+L["color_option_3"] = "static color"
+L["color_option_4"] = "reaction color"
+--Status Bars
+L["Health Bar"] = true
+L["Power Bar"] = true
+--Vocabulary
+L["Name"] = true
+
+
+L[""] = true
+        
