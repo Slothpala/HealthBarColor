95c44094593a572ae6522869ad89755d6db1d881
Slothpala <slothpala@gmail.com>
2023-08-14 23:21:57 +0200

    Localization support for Fonts anad Enabled Modules tab added

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -11,6 +11,42 @@ L["outline_option_1"] = "OUTLINE"
 L["outline_option_2"] = "THICKOUTLINE"
 L["outline_option_3"] = "MONOCHROME"
 L["outline_option_4"] = "NONE"
+--Textures
+L["Texture"] = true
+--BackgroundTextures
+L["Background Texture"] = true
+--DebuffColor
+L["DebuffColor"] = true
+--PartyColor
+L["PartyColor"] = true
+--Glow
+L["Glow"] = true
+--HideClassPowerBar
+L["HideClassPowerBar"] = true
+--BiggerHealthBar
+L["BiggerHealthBar"] = true
+--Overabsorb
+L["Overabsorb"] = true
+--Settings/Module Selection
+L["Enabled Modules"] = true
+L["Modules"] = true
+L["fonts_desc"] = "change the font colors of health bars"
+L["tex_desc"] = "select custom health bar textures"
+L["bg_tex_desc"] = "create background textures beneath health bars"
+L["debuff_desc"] = "color the player health bar by debuff type if you can dispel yourself"
+L["party_desc"] = "enables class colors for the defaulft non raid style party frames."
+L["glow_desc"] = "change or hide the the glowing colors of the player, target & focus frame."
+L["hide_power_desc"] = "Hide the class power bar."
+L["bigger_hb_desc"] = "Resizes the player's health bar to cover the entire inner player frame."
+L["overabsorb_desc"] = "Show absorb values above max health for player target and focus frame."
+L["Overwrite Class Colors"] = true
+L["Custom Class Colors"] = true
+L["custom_class_color_desc"] = "don't like a class color? change it!"
+L["Overwrite Reaction Colors"] = true
+L["Custom Reaction Colors"] = true
+L["Hostile"] = true
+L["Neutral"] = true
+L["Friendly"] = true
 --Units
 L["Player"] = true
 L["Target"] = true
@@ -19,6 +55,20 @@ L["Focus"] = true
 L["Target of Focus"] = true
 L["Pet"] = true
 L["Boss"] = true
+--Classes
+L["DK"] = "Death Knights"
+L["DH"] = "Demon Hunter"
+L["DRUID"] = "Druid"
+L["EVO"] = "Evoker"
+L["HUNT"] = "Hunter"
+L["MAGE"] = "Mage"
+L["MONK"] = "Monk"
+L["PAL"] = "Paladin"
+L["PRST"] = "Priest"
+L["ROG"] = "Rogue"
+L["SHAM"] = "Shaman"
+L["WL"] = "Warlock"
+L["WAR"] = "Warrior"
 --Color options
 L["color"] = true
 L["NPC color"] = true
@@ -36,7 +86,8 @@ L["Health Bar"] = true
 L["Power Bar"] = true
 --Vocabulary
 L["Name"] = true
-
+L["reset"] = true
+L["to default"] = true
 
 L[""] = true
         
