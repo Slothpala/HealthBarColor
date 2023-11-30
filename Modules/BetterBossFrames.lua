a944093f4eb47bfb8d683379157fd173792a89ac
Slothpala <slothpala@gmail.com>
2023-11-30 03:07:25 +0100

    textures updated

--- Modules/BetterBossFrames.lua
+++ Modules/BetterBossFrames.lua
@@ -13,32 +13,41 @@ local BetterBossFrames = HealthBarColor:NewModule("BetterBossFrames")
 Mixin(BetterBossFrames, addonTable.hooks)
 local Textures = {
 	FrameTexture = {
-		path = "Interface\\AddOns\\HealthBarColor\\Textures\\UnitFrames\\Boss\\UIUnitFrameBoss_small.png",
+		path = "Interface\\AddOns\\HealthBarColor\\Textures\\UnitFrames\\Boss\\BetterBossFrames.png",
 		coords = {0.3984375, 0.84765625, 0.00390625, 0.22265625},
 	},
 	FrameFlash = {
-		path = "Interface\\AddOns\\HealthBarColor\\Textures\\UnitFrames\\Boss\\UIUnitFrameBoss_small.png",
-		coords = {0.32421875, 0.66015625, 0.328125, 0.328125},
+		path = "Interface\\AddOns\\HealthBarColor\\Textures\\UnitFrames\\Boss\\BetterBossFlash.png",
+		coords = {0.3984375, 0.84765625, 0.00390625, 0.22265625},
+	},
+	Mask = {
+		path = "Interface\\AddOns\\HealthBarColor\\Textures\\UnitFrames\\Boss\\MaskTextureBoss.png",
+		coords = {22/128, 106/128, 21/64, 43/64},
 	},
 }
+
+
 
 local function replaceTextures(hbc_unit)
 	hbc_unit.FrameTexture:SetTexture(Textures.FrameTexture.path)
 	hbc_unit.FrameTexture:SetTexCoord(unpack(Textures.FrameTexture.coords))
 	hbc_unit.FrameFlash:SetTexture(Textures.FrameFlash.path)
 	hbc_unit.FrameFlash:SetTexCoord(unpack(Textures.FrameFlash.coords))
+	hbc_unit.FrameFlash:SetAllPoints(hbc_unit.FrameTexture)
 end
 
 local function resizeHealthBar(hbc_unit)
 	hbc_unit.Skull:SetAlpha(0)
 	hbc_unit.Glow:Hide()
-	hbc_unit.HealthBar.HealthBarMask:SetAtlas("UI-HUD-UnitFrame-Party-PortraitOff-Bar-Health-Mask")
-	hbc_unit.HealthBar.HealthBarMask:SetPoint("TOPLEFT",hbc_unit.HealthBar,-23,10)
-	hbc_unit.HealthBar.HealthBarMask:SetPoint("BOTTOMRIGHT",hbc_unit.HealthBar,23,-10)
+	hbc_unit.HealthBar.HealthBarMask:SetTexture(Textures.Mask.path)
+	hbc_unit.HealthBar.HealthBarMask:SetPoint("TOPLEFT",hbc_unit.HealthBar,-21,22)
+	hbc_unit.HealthBar.HealthBarMask:SetPoint("BOTTOMRIGHT",hbc_unit.HealthBar,23,-22)
 	hbc_unit.HealthBar:SetHeight(22)
 	hbc_unit.Name:ClearAllPoints()
-	hbc_unit.Name:SetPoint("CENTER", hbc_unit.Glow, "CENTER",0,14)
-	hbc_unit.Name:SetJustifyH("CENTER")
+	hbc_unit.Name:SetWidth(65)
+	hbc_unit.Name:SetPoint("TOPLEFT", hbc_unit.Glow, "TOPLEFT",2,14)
+	hbc_unit.LevelText:ClearAllPoints()
+	hbc_unit.LevelText:SetPoint("TOPRIGHT", hbc_unit.Glow, "TOPRIGHT",0,14)
 end
 
 function BetterBossFrames:OnEnable()
